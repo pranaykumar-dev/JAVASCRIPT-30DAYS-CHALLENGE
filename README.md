134cc250b9bc3da9388c5c3742b462d87dcb9492
prani14 <pranaykumarshivas1@gmail.com>
2024-07-13 19:56:01 +0530

    day1-challege-completed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JAVASCRIPT-30DAYS-CHALLENGE
 Here I upload all my 30days javascript challenge solutions 
+## i have completed my 1st day challenge (13-7-24) 
---
+++ variables_&&dataTypes.js
@@ -0,0 +1,54 @@
+// declare a variable using var
+var UserNumber = 21
+console.log(UserNumber);
+
+// declare a variable using let 
+let UserName = "pranay"
+console.log(UserName);
+
+// constant declaration 
+const booLean = true 
+console.log(booLean);
+
+// data types 
+let number = 21
+let name = "kumar"
+let boolean = true 
+let username = {
+    userId:12
+}
+const UserIs =[1,2,3,4,5]
+
+// print 
+console.log( "DataType Is",  typeof number);
+// variables output 
+console.log("variables assigned", number);
+console.log( "DataType Is", typeof name);
+// variables output 
+console.log("variables assigned", name);
+console.log( "DataType Is", typeof boolean );
+// variables output 
+console.log("variables assigned", boolean);
+console.log( "DataType Is", typeof username );
+// variables output 
+console.log("variables assigned",username);
+console.log( "DataType Is", typeof UserIs);
+console.log("variables assigned",UserIs);
+
+
+// reassigning variables 
+let Name = "pranay"
+Name = "PRANAY"
+console.log(Name);
+
+// reassigning variables 
+const MyName = "PranayKumar"
+// MyName = "kumar"
+console.log(MyName);
+// ERROR !
+// TypeError: Assignment to constant variable.
+
+
+
+
+
