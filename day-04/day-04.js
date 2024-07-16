a50d386ef6c9b628a3400a4e6e61577c81580e81
Pranay kumar <166115244+pranaykumar-dev@users.noreply.github.com>
2024-07-16 13:39:26 +0000

    challenge completed

--- day-04/day-04.js
+++ day-04/day-04.js
@@ -1,25 +1,25 @@
 // for loop repeats until a specified condition evaluates to false. 
 //The JavaScript for loop is similar to the Java and C for loop.
 // to find the numbers from 1 to 10 
+
 for (let index = 1; index <= 10; index++) {
     console.log(index);
     
 }
 // to find multiple of 5 
+console.log("to find multiple of 5");   
 for (let index = 1; index < 10; index++) {
     console.log(5*index );
     index+1
     
 }
-
+console.log("sum of ");
 value = 0
 counter = 1
 while(counter<=10){
     value += counter
     counter++
     console.log("sum is ",value)
-     
-
 }
 i = 10
 while(i>=1){
@@ -46,3 +46,17 @@ do {
     console.log(mul);
     
  } while (number >ina);
+
+ console.log("pattern");
+
+ let n = 5
+
+ for (let row = 1; row <= n; row++) {
+    let pattern = '';
+
+for (let col = 1; col <= row; col++) {
+        pattern += '* ';
+    }
+    console.log(pattern);   
+ }
+ 
