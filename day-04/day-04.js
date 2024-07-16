47d5957ca511f9e48359239b3b44c056de2c4ead
Pranay kumar <166115244+pranaykumar-dev@users.noreply.github.com>
2024-07-16 07:28:51 +0000

    16-07-2024

---
+++ day-04/day-04.js
@@ -0,0 +1,48 @@
+// for loop repeats until a specified condition evaluates to false. 
+//The JavaScript for loop is similar to the Java and C for loop.
+// to find the numbers from 1 to 10 
+for (let index = 1; index <= 10; index++) {
+    console.log(index);
+    
+}
+// to find multiple of 5 
+for (let index = 1; index < 10; index++) {
+    console.log(5*index );
+    index+1
+    
+}
+
+value = 0
+counter = 1
+while(counter<=10){
+    value += counter
+    counter++
+    console.log("sum is ",value)
+     
+
+}
+i = 10
+while(i>=1){
+    console.log(i);
+    i-=1
+
+}
+
+i = 1
+console.log("do while Loop");
+do {
+    console.log(i);
+    i+=1
+} while (i<=5);
+
+ console.log("factorial");
+ let number = 5
+ let ina =0
+ do {
+    let mul = number * ina
+    number--
+    ina++
+    // console.log(ina);
+    console.log(mul);
+    
+ } while (number >ina);
